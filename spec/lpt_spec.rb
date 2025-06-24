056d4f798a374fa089b0c1a69debe2ac350ff470
Chris Woodford <chris.woodford@gmail.com>
2025-06-24 10:54:00 -0400

    Setup Bare Gem

---
+++ spec/lpt_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Lpt::Ruby do
+  it "has a version number" do
+    expect(Lpt::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
