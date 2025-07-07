ce4b8d7d7b00498fcbb9c65419fa22473c9498e3
Chris Woodford <chris.woodford@gmail.com>
2025-07-07 12:23:48 -0400

    Improve Working with Payment Requests
    This commit introduces some a few methods on the Payment Request class
    to cover some of the specific use cases that payments have. The end
    result is a more fluent API.
    
    1. Two of the methods are a refactoring of the previous workflow
       implementation. Rather than leak the constants out into the Payment
       resource class, I opted to encapulate their usage behind methods that
       clearly describe their intent.
    2. Adding a method to mark a request as a being initiated by the
       merchant rather than by the customer (customer initiation is the
       default). The core use case here is recurring payments.
    
    The expected usage will could look something like this:
    
    ```
    Lpt::Requests::PaymentRequest.
      new.
      as_recurring_payment.
      as_sale
    ```

--- spec/lpt/requests/payment_request_spec.rb
+++ spec/lpt/requests/payment_request_spec.rb
@@ -11,4 +11,61 @@ RSpec.describe Lpt::Requests::PaymentRequest do
       expect(result.merchant).to eq("LMR123123123")
     end
   end
+
+  describe "#as_auth_capture" do
+    it "sets the workflow to auth/capture" do
+      auth_capture = Lpt::Requests::PaymentRequest::WORKFLOW_AUTH_CAPTURE
+      request = Lpt::Requests::PaymentRequest.new
+
+      request.as_auth_capture
+
+      expect(request.workflow).to eq(auth_capture)
+    end
+
+    it "supports method chaining" do
+      request = Lpt::Requests::PaymentRequest.new
+
+      result = request.as_auth_capture
+
+      expect(result).to eq(request)
+    end
+  end
+
+  describe "#as_sale" do
+    it "sets the workflow to sale" do
+      sale = Lpt::Requests::PaymentRequest::WORKFLOW_SALE
+      request = Lpt::Requests::PaymentRequest.new
+
+      request.as_sale
+
+      expect(request.workflow).to eq(sale)
+    end
+
+    it "supports method chaining" do
+      request = Lpt::Requests::PaymentRequest.new
+
+      result = request.as_sale
+
+      expect(result).to eq(request)
+    end
+  end
+
+  describe "#as_recurring_payment" do
+    it "sets initiation by the merchant" do
+      initiated_by_merchant = Lpt::Requests::PaymentRequest::INITIATION_MERCHANT
+      request = Lpt::Requests::PaymentRequest.new
+
+      request.as_recurring_payment
+
+      expect(request.initiation).to eq(initiated_by_merchant)
+    end
+
+    it "supports method chaining" do
+      request = Lpt::Requests::PaymentRequest.new
+
+      result = request.as_recurring_payment
+
+      expect(result).to eq(request)
+    end
+  end
 end
