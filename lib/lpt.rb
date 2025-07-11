0ac0b845946593313b22cba0a1657c802b2e8c0b
Chris Woodford <chris.woodford@gmail.com>
2025-07-10 21:42:31 -0400

    Resolve Failing Tests with Base Addresses

--- lib/lpt.rb
+++ lib/lpt.rb
@@ -67,10 +67,12 @@ module Lpt
     end
 
     def base_addresses(environment: nil)
-      if environment == Lpt::Environment::PRODUCTION
+      case environment
+      when Lpt::Environment::PRODUCTION
         { api_base: "api-s2", cx_base: "cx", cx_api_base: "api.cx" }
-      elsif environment == Lpt::Environment::STAGING
-        { api_base: "staging-api-s2", cx_base: "cx.stg", cx_api_base: "api.cx.stg" }
+      when Lpt::Environment::STAGING
+        { api_base: "staging-api-s2", cx_base: "cx.stg",
+          cx_api_base: "api.cx.stg" }
       else
         { api_base: "api", cx_base: "cx", cx_api_base: "api.cx" }
       end
